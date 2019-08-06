92e86cd686f38fb7b94016e397f82dcf69199ef8
tatiana faramarzi <tfaramarzi@gmail.com>
2019-08-06 14:50:02 -0700

    Created files for components and added constructor and render functions to App.js

--- github-project/src/App.js
+++ github-project/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  constructor() {
+    console.log("CONSTRUCTOR INVOKED");
+    super();
+    this.state = {
+      user: userData
+    };
+  }
+
+  // componentDidMount(){
+
+  // }
+
+  render(){
+    console.log("RENDER INVOKED")
+    return (
+      <div className="App">
+        <h1>All About This GitHub</h1>
+        
+      </div>
+    );
+  }
+  
+
 }
+
 
 export default App;
